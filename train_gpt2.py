01dd64febf421b03a909f5e10e0e20869933272c
AlphaBrain <weirenwang0@gmail.com>
2024-08-10 22:47:06 -0700

    Add MLP and Block

--- train_gpt2.py
+++ train_gpt2.py
@@ -3,6 +3,33 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as f
 
+
+class MLP(nn.module):
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc = nn.Linear(config.n_emb, 4 * config.n_emb)
+        self.gelu = nn.GELU(approximate='tanh')
+        self.c_proj = nn.Linear(4 * config.n_emb, config.n_emb)
+    
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        return x
+
+class Block(nn.module):
+    def __init__(self, config):
+        super().__init__()
+        self.ln_1 = nn.LayerNorm(config.n_emb)
+        self.attn = CauselSelfAttention(config)
+        self.ln_2 = nn.LayerNorm(config.n_emb)
+        self.mlp = MLP(config)
+    
+    def forward(self, x):
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
+
 @dataclass
 class GPTConfig:
     block_size: int = 256
