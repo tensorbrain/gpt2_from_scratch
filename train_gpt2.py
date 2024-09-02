f0ec3f977ab7cfaaeaa8f4abe85c273b9d6bd5c5
AlphaBrain <weirenwang0@gmail.com>
2024-09-01 20:20:53 -0700

    add Dataloadlite class

--- train_gpt2.py
+++ train_gpt2.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 import torch
 import torch.nn as nn
 from torch.nn import functional as f
+import os
 
 
 class CausalSelfAttention(nn.Module):
@@ -157,3 +158,51 @@ class GPT(nn.module):
                     sd[k].copy_(sd_hf[k])
 
         return model
+
+import tiktoken
+import numpy as np
+def load_tokens(filename):
+    npt = np.load(filename)
+    npt = np.astype(np.int32)
+    ptt = torch.tensor(npt, dtype=torch.long)
+
+class DataLoaderLite:
+    def __init__(self, B, T, process_rank, num_processes, split):
+        assert split in ['train', 'eval']
+        self.B = B
+        self.T = T
+        self.process_rank = process_rank
+        self.num_processes = num_processes
+    
+        data_root = "edu_fineweb10B"
+        shards = os.listdir(data_root)
+        shards = [s for s in shards if split in s]
+        shards = sorted(shards)
+        shards = [os.path.join(data_root, s) for s in shards]
+        self.shards = shards
+        assert len(shard) > 0, f"no shard found for split {split}"
+        if master_process:
+            print(f"found {len(shards)} for split {split}")
+        self.reset()
+
+    def reset():
+        self.current_shard = 0
+        self.tokens = load_tokens(self.shards[self.current_shard])
+        self.current_position = self.B * self.T * self.process_rank
+    
+    def next_batch(self):
+        B, T = self.B, self.T
+        buf = self.tokens[self.current_position, self.current_position + B * T * num_processes]
+        x = buf[:-1].view(B, T)
+        y = buf[1:].view(B, T)
+        self.current_position += B * T * num_processes
+        if(self.current_position + B * T * num_processes +1 > len(tokens)):
+            self.current_shard = (self.current_shard+1) % len(shards)
+            self.tokens = load_tokens(self.shards[self.current_shard])
+            current_position = self.B * self.T * self.process_rank
+        return x, y
+
+         
+
+
+
